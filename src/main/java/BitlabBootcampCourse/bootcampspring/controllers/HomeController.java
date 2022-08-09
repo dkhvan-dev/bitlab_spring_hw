8455a799381aa4fafb40cdafd4c7cfc13a0f8672
Denis Khvan <iddk_kz@mail.ru>
2022-08-09 21:34:58 +0600

    Thymeleaf. Task third-one

--- src/main/java/BitlabBootcampCourse/bootcampspring/controllers/HomeController.java
+++ src/main/java/BitlabBootcampCourse/bootcampspring/controllers/HomeController.java
@@ -1,12 +1,12 @@
 package BitlabBootcampCourse.bootcampspring.controllers;
 
 import BitlabBootcampCourse.bootcampspring.db.DBManager;
-import BitlabBootcampCourse.bootcampspring.model.Item;
+import BitlabBootcampCourse.bootcampspring.db.Students;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 
@@ -14,37 +14,36 @@ import java.util.ArrayList;
 public class HomeController {
 
     @GetMapping(value = "/")
-    public String indexPage(Model model) {
-        ArrayList<Item> items = DBManager.getItems();
-        model.addAttribute("items", items);
+    public String index(Model model) {
+        ArrayList<Students> students = DBManager.getAllStudents();
+        model.addAttribute("students", students);
         return "index";
     }
 
-    @GetMapping(value = "/addItem")
-    public String addItemGet() {
-        return "addItem";
+    @GetMapping(value = "/addStudent")
+    public String addStudentPage(Model model) {
+        return "addStudent";
     }
 
-    @PostMapping(value = "/addItem")
-    public String addItemPost(@RequestParam(name = "item_title") String name,
-                              @RequestParam(name = "item_price") double price,
-                              @RequestParam(name = "item_description") String description) {
-        Item item = new Item();
-        item.setName(name);
-        item.setPrice(price);
-        item.setDescription(description);
-        DBManager.addItem(item);
+    @PostMapping(value = "/addStudent")
+    public String addStudent(Students student,
+                             Model model) {
+        if (student != null) {
+            DBManager.addStudent(student);
+            model.addAttribute("student", student);
+        }
         return "redirect:/";
-
     }
 
-    @GetMapping(value = "/details")
-    public String details(@RequestParam(name = "id") Long id,
-            Model model) {
-        Item item = DBManager.getItem(id);
-        System.out.println(id);
-        model.addAttribute("item", item);
+    @GetMapping(value = "/details/{studentId}/{studentName}")
+    public String details(@PathVariable(name = "studentId") Long student_id,
+                          @PathVariable(name = "studentName") String student_name,
+                          Model model) {
+        Students student = DBManager.getStudent(student_id);
+        if (student != null && !student.getStudent_name().equals(student_name)) {
+            return "redirect:/details/" + student.getStudent_id() + "/" + student.getStudent_name() + ".html";
+        }
+        model.addAttribute("student", student);
         return "details";
     }
-
 }
