012d03c258afe51a55baf40f22ad5fb64f1ee092
Denis Khvan <iddk_kz@mail.ru>
2022-08-10 13:05:04 +0600

    Thymeleaf Layout Dialect

--- src/main/java/BitlabBootcampCourse/bootcampspring/controllers/HomeController.java
+++ src/main/java/BitlabBootcampCourse/bootcampspring/controllers/HomeController.java
@@ -1,12 +1,13 @@
 package BitlabBootcampCourse.bootcampspring.controllers;
 
 import BitlabBootcampCourse.bootcampspring.db.DBManager;
-import BitlabBootcampCourse.bootcampspring.db.Students;
+import BitlabBootcampCourse.bootcampspring.model.Tasks;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 
@@ -15,35 +16,51 @@ public class HomeController {
 
     @GetMapping(value = "/")
     public String index(Model model) {
-        ArrayList<Students> students = DBManager.getAllStudents();
-        model.addAttribute("students", students);
+        ArrayList<Tasks> tasks = DBManager.getAllTasks();
+        model.addAttribute("tasks", tasks);
         return "index";
     }
 
-    @GetMapping(value = "/addStudent")
-    public String addStudentPage(Model model) {
-        return "addStudent";
-    }
-
-    @PostMapping(value = "/addStudent")
-    public String addStudent(Students student,
-                             Model model) {
-        if (student != null) {
-            DBManager.addStudent(student);
-            model.addAttribute("student", student);
-        }
+    @PostMapping(value = "/addTask")
+    public String addTask(@RequestParam(name = "name") String name,
+            @RequestParam(name = "description") String description,
+            @RequestParam(name = "deadlineDate") String deadlineDate,
+            @RequestParam(name = "isCompleted") boolean isCompleted) {
+        Tasks task = new Tasks();
+        task.setName(name);
+        task.setDescription(description);
+        task.setDeadlineDate(deadlineDate);
+        task.setCompleted(isCompleted);
+        DBManager.addTask(task);
         return "redirect:/";
     }
 
-    @GetMapping(value = "/details/{studentId}/{studentName}")
-    public String details(@PathVariable(name = "studentId") Long student_id,
-                          @PathVariable(name = "studentName") String student_name,
+    @GetMapping(value = "/details/{taskId}")
+    public String details(@PathVariable(name = "taskId") Long id,
                           Model model) {
-        Students student = DBManager.getStudent(student_id);
-        if (student != null && !student.getStudent_name().equals(student_name)) {
-            return "redirect:/details/" + student.getStudent_id() + "/" + student.getStudent_name() + ".html";
-        }
-        model.addAttribute("student", student);
+        Tasks task = DBManager.getTask(id);
+        model.addAttribute("task", task);
         return "details";
+    }
+
+    @PostMapping(value = "/details")
+    public String updateTask(@RequestParam(name = "id") Long id,
+                             @RequestParam(name = "name") String name,
+                             @RequestParam(name = "description") String description,
+                             @RequestParam(name = "deadlineDate") String deadlineDate,
+                             @RequestParam(name = "isCompleted") boolean isCompleted) {
+        Tasks task = DBManager.getTask(id);
+        task.setName(name);
+        task.setDescription(description);
+        task.setDeadlineDate(deadlineDate);
+        task.setCompleted(isCompleted);
+        DBManager.updateTask(task);
+        return "redirect:/details/" + id;
+    }
+
+    @PostMapping(value = "/deleteTask")
+    public String deleteTask(@RequestParam(name = "id") Long id) {
+        DBManager.deleteTask(id);
+        return "redirect:/";
     }
 }
--- src/main/java/BitlabBootcampCourse/bootcampspring/db/DBManager.java
+++ src/main/java/BitlabBootcampCourse/bootcampspring/db/DBManager.java
@@ -1,42 +1,53 @@
 package BitlabBootcampCourse.bootcampspring.db;
 
+import BitlabBootcampCourse.bootcampspring.model.Tasks;
+import org.springframework.scheduling.config.Task;
+
 import java.util.ArrayList;
 
 public class DBManager {
-
-    private static ArrayList<Students> students = new ArrayList<>();
-    private static Long id = 8L;
+    private static ArrayList<Tasks> tasks = new ArrayList<>();
+    private static Long id = 6L;
 
     static {
-        students.add(new Students(1L, "Ilyas", "Zhuanyshev", 88, ' '));
-        students.add(new Students(2L, "Serik", "Erikov", 91, ' '));
-        students.add(new Students(3L, "Erik", "Serikov", 65, ' '));
-        students.add(new Students(4L, "Nurzhan", "Bolatov", 48, ' '));
-        students.add(new Students(5L, "Patrick", "Zuckerberg", 100, ' '));
-        students.add(new Students(6L, "Sabina", "Assetova", 33, ' '));
-        students.add(new Students(7L, "Madina", "Adletova", 77, ' '));
+        tasks.add(new Tasks(1L, "Complete Task 7 from Spring Boot till the end of lesson", "Complete Task 7 from Spring Boot till the end of lesson", "23.10.2020", true));
+        tasks.add(new Tasks(2L, "Clear home and buy foods", "Clear home and buy foods", "25.10.2020", true));
+        tasks.add(new Tasks(3L, "Complete all home tasks at the weekend", "Complete all home tasks at the weekend", "28.10.2020", false));
+        tasks.add(new Tasks(4L, "Develop simple project in Spring Boot for the final", "Develop simple project in Spring Boot for the final", "12.12.2020", false));
+        tasks.add(new Tasks(5L, "Learn Italian Language", "Learn Italian Language", "01.05.2021", false));
     }
 
-    public static ArrayList<Students> getAllStudents() {
-        for (Students student : students) {
-            if (student != null) {
-                if (student.getStudent_exam() >= 90) student.setStudent_mark('A'); else
-                    if (student.getStudent_exam() >= 75 && student.getStudent_exam() <= 89) student.setStudent_mark('B'); else
-                        if (student.getStudent_exam() >= 60 && student.getStudent_exam() <= 74) student.setStudent_mark('C'); else
-                            if (student.getStudent_exam() >= 50 && student.getStudent_exam() <= 59) student.setStudent_mark('D'); else student.setStudent_mark('F');
+    public static ArrayList<Tasks> getAllTasks() {
+        return tasks;
+    }
+
+    public static Tasks getTask(Long id) {
+        return tasks.stream().filter(task -> task.getId().equals(id)).findFirst().orElse(null);
+    }
+
+    public static void addTask(Tasks task) {
+        task.setId(id);
+        tasks.add(task);
+        id++;
+    }
+
+    public static void updateTask(Tasks task) {
+        for (Tasks t : tasks) {
+            if (t.getId().equals(task.getId())) {
+                t.setName(task.getName());
+                t.setDescription(task.getDescription());
+                t.setDeadlineDate(task.getDeadlineDate());
+                t.setCompleted(task.isCompleted());
             }
         }
-        return students;
     }
 
-    public static Students getStudent(Long id) {
-        return students.stream().filter(student -> student.getStudent_id().equals(id)).findFirst().orElse(null);
-    }
-
-    public static Students addStudent(Students student) {
-        student.setStudent_id(id);
-        students.add(student);
-        id++;
-        return student;
+    public static void deleteTask(Long id) {
+        for (int i = 0; i < tasks.size(); i++) {
+            if (tasks.get(i).getId().equals(id)) {
+                tasks.remove(i);
+                break;
+            }
+        }
     }
 }
